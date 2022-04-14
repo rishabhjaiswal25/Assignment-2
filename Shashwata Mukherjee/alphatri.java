8c4e8bd167b5435821864b5112cf64f329cf26d4
Shashwata <41300641+shashwatamukherjee@users.noreply.github.com>
2022-04-14 18:11:23 +0530

    Add files via upload

---
+++ Shashwata Mukherjee/alphatri.java
@@ -0,0 +1,16 @@
+package Assignment2;
+
+public class alphatri {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=65;i<75;i++) {
+			for(int j=65;j<=i;j++) {
+				System.out.print((char)j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
