8b5802491cdd8790e3c279d73ed03983d1e9f32d
Sahauday <103258047+Sahauday@users.noreply.github.com>
2022-04-17 11:45:58 +0530

    Create 5.java

---
+++ Uday_Saha/5.java
@@ -0,0 +1,13 @@
+public class my {
+
+	public static void main(String[] args) {
+		for (int p=2;p<12;p++) {
+			for(int q=2;q<=p;q++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
