715e3ea05227cae20e7d0c8b81a8960be9719e79
jaswanth1009 <103258802+jaswanth1009@users.noreply.github.com>
2022-04-17 09:28:55 +0530

    Create 06.java

---
+++ jaswanth_assignement_2/06.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+		char alpha='A';
+		int i,j,n=5;
+		for(i=1;i<=n;i++) {
+			alpha='A';
+			for(j=1;j<=i;j++) {
+				System.out.print(alpha +" ");
+				alpha++;
+			}
+			System.out.println();
+			
+		}
+	
+
+	}
+
+}
