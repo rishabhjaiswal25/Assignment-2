8babe63fa15639abd4bb947d067612424c5ed533
Surendra99210 <103258715+Surendra99210@users.noreply.github.com>
2022-04-17 22:15:23 +0530

    Add files via upload

---
+++ Surendrakumar_M/max_num.java
@@ -0,0 +1,49 @@
+package new_pakage;
+import java.util.*;
+
+public class max_num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n, max;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the limit:");
+
+        n = s.nextInt();
+
+        int a[] = new int[n];
+
+        System.out.println("Enter the numbers:");
+
+        for(int i = 0; i < n; i++)
+
+        {
+
+            a[i] = s.nextInt(); 
+
+        }
+
+        max = a[0];
+
+        for(int i = 0; i < n; i++)
+
+        {
+
+            if(max < a[i])
+
+            {
+
+                max = a[i];
+
+            }
+
+        }
+
+        System.out.println("Maximum NUmber is:"+max); 
+
+
+	}
+	
+}
