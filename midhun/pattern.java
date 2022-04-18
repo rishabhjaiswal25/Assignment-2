7aca9f4cf76b7a127735bed8f34e5793409cf073
midhun-kuppili <103258440+midhun-kuppili@users.noreply.github.com>
2022-04-18 10:34:56 +0530

    Create pattern.java

---
+++ midhun/pattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class number_triangle 
+{
+	public static void main(String[] args)
+	{
+		for(int i=0;i<10;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+}
